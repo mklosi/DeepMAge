e7ef7fc23849909735ff6c24fc612e73aecda1d1
Martin Klosi <roise3000@gmail.com>
2024-11-27 16:20:28 -0800

    intro download_resources_methylprep.sh

--- download_resources_methylprep.sh
+++ download_resources_methylprep.sh
@@ -4,12 +4,14 @@ set -e
 ## errors: 
 # GSE128235 - ValueError: Not an IDAT file. Unsupported file type.
 # GSE105123 - FileNotFoundError: [Errno 2] No such file or directory: 'resources_methylprep/GSE105123_download_pandas_1.3.5/GSE105123_family.xml'
+# GSE106648 - ERROR:methylprep.download.process_data:[!] Geo data set GSE106648 probably does NOT contain usable raw data (in .idat format). Not downloading.
+# GSE77696 - ERROR:methylprep.download.process_data:[!] Geo data set GSE77696 probably does NOT contain usable raw data (in .idat format). Not downloading.
 
 for gse_id in GSE59065 GSE106648 GSE61496 GSE107459 GSE77696
 do
     echo "--- $gse_id ------------------"
     python -m methylprep -v download -i $gse_id -d resources_methylprep/"$gse_id"_download_pandas_1.3.5
     echo "AAA done with download"
-    rm -rf resources_methylprep/$gse_id_download_pandas_1.3.5/*/*.idat
+    rm -rf resources_methylprep/"$gse_id"_download_pandas_1.3.5/*/*.idat
     echo "AAA done with delete"
 done
