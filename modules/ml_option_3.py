322b89fedce92b4a6efd1abd56448e1c52f3d4b7
Martin Klosi <roise3000@gmail.com>
2024-11-30 15:03:50 -0800

    successfully implemented my own dynamic solution to layers

--- modules/ml_option_3.py
+++ modules/ml_option_3.py
@@ -72,73 +72,72 @@ set_seeds()
 #         return x
 
 
-class DeepMAgeModel(nn.Module):
-    """Deep neural network for age prediction with configurable inner layers."""
-
-    def __init__(self, config):
-        super(DeepMAgeModel, self).__init__()
-        self.config = config
-        inner_layers = self.config["inner_layers"]
-        dropout = self.config["dropout"]
-        activation_funcs = {
-            "elu": nn.ELU(),
-            "relu": nn.ReLU(),
-        }
-        activation_func = activation_funcs[self.config["activation_func"]]
-
-        # Create the layers dynamically
-        layers = []
-        input_dim = self.config["input_dim"]
-        for i, layer_dim in enumerate(inner_layers):
-            layers.append(nn.Linear(input_dim, layer_dim))
-            layers.append(activation_func)
-            # Add dropout only if not the final hidden layer
-            if i < len(inner_layers) - 1:
-                layers.append(nn.Dropout(dropout))
-            input_dim = layer_dim
-
-        # Add the final layer (output layer)
-        layers.append(nn.Linear(input_dim, 1))
-
-        # Use nn.Sequential to combine all layers into a single module
-        self.network = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.network(x)
-
-
 # class DeepMAgeModel(nn.Module):
-#     """Deep neural network for age prediction."""
+#     """Deep neural network for age prediction with configurable inner layers."""
 #
 #     def __init__(self, config):
 #         super(DeepMAgeModel, self).__init__()
 #         self.config = config
-#
-#         # Dynamically create layers.
-#         self.fcs = [nn.Linear(self.config["input_dim"], self.config["inner_layers"][0])]
-#         previous_out = self.config["inner_layers"][0]
-#         for inner_layer in self.config["inner_layers"][1:]:
-#             self.fcs.append(nn.Linear(previous_out, inner_layer))
-#             previous_out = inner_layer
-#         self.fcs.append(nn.Linear(previous_out, 1))
-#
-#         self.dropout = nn.Dropout(self.config["dropout"])
+#         inner_layers = self.config["inner_layers"]
+#         dropout = self.config["dropout"]
 #         activation_funcs = {
 #             "elu": nn.ELU(),
 #             "relu": nn.ReLU(),
 #         }
-#         self.activation_func = activation_funcs[self.config["activation_func"]]
+#         activation_func = activation_funcs[self.config["activation_func"]]
 #
-#         fdjkfd = 1 # &&&
+#         # Create the layers dynamically
+#         layers = []
+#         input_dim = self.config["input_dim"]
+#         for i, layer_dim in enumerate(inner_layers):
+#             layers.append(nn.Linear(input_dim, layer_dim))
+#             layers.append(activation_func)
+#             # Add dropout only if not the final hidden layer
+#             if i < len(inner_layers) - 1:
+#                 layers.append(nn.Dropout(dropout))
+#             input_dim = layer_dim
+#
+#         # Add the final layer (output layer)
+#         layers.append(nn.Linear(input_dim, 1))
+#
+#         # Use nn.Sequential to combine all layers into a single module
+#         self.network = nn.Sequential(*layers)
 #
 #     def forward(self, x):
-#         for i, fc in enumerate(self.fcs[:-1]):
-#             x = self.activation_func(fc(x))
-#             # Add dropout only if not the final hidden layer
-#             if i < len(self.fcs[:-1]) - 1:
-#                 x = self.dropout(x)
-#         x = self.fcs[-1](x)
-#         return x
+#         return self.network(x)
+
+
+class DeepMAgeModel(nn.Module):
+    """Deep neural network for age prediction."""
+
+    def __init__(self, config):
+        super(DeepMAgeModel, self).__init__()
+        self.config = config
+
+        # Dynamically create layers.
+        self.fcs = nn.ModuleList()
+        self.fcs.append(nn.Linear(self.config["input_dim"], self.config["inner_layers"][0]))
+        previous_out = self.config["inner_layers"][0]
+        for inner_layer in self.config["inner_layers"][1:]:
+            self.fcs.append(nn.Linear(previous_out, inner_layer))
+            previous_out = inner_layer
+        self.fcs.append(nn.Linear(previous_out, 1))
+
+        self.dropout = nn.Dropout(self.config["dropout"])
+        activation_funcs = {
+            "elu": nn.ELU(),
+            "relu": nn.ReLU(),
+        }
+        self.activation_func = activation_funcs[self.config["activation_func"]]
+
+    def forward(self, x):
+        for i, fc in enumerate(self.fcs[:-1]):
+            x = self.activation_func(fc(x))
+            # Add dropout only if not the final hidden layer
+            if i < len(self.fcs[:-1]) - 1:
+                x = self.dropout(x)
+        x = self.fcs[-1](x)
+        return x
 
 
 class MedAELoss(nn.Module):
