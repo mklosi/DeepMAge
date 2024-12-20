771e5a195cb0e5cd28c7f1897aff8c196882071b
Martin Klosi <roise3000@gmail.com>
2024-12-20 10:04:49 -0800

    study_name = "study-17" (mklosi)

--- modules/ml_optuna_1.py
+++ modules/ml_optuna_1.py
@@ -49,7 +49,7 @@ results_base_path = "result_artifacts"
 
 # &&& param
 # study_name = get_config_id(search_space)[:16]  # Half of actual length.
-study_name = "study-15"
+study_name = "study-17"
 
 study_db_url = f"sqlite:///{results_base_path}/studies.db"
 lock_path = Path(f"{results_base_path}/result_df.lock")
